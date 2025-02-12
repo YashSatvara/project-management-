f7ea9abd38d206eddc5ca84f5be40eac7207b71d
yashs <yashsathvara2003@gmail.com>
2025-02-12 10:52:31 +0530

    server modification

--- server/src/index.ts
+++ server/src/index.ts
@@ -32,7 +32,7 @@ app.use("/project",projectRoutes);
 app.use("/tasks",taskRoutes);
 
 /*SERVER*/
-const port = process.env.PORT || 3000;
-app.listen(port, ()=>{
+const port =Number(process.env.PORT)|| 3000;
+app.listen(port, "0.0.0.0",()=>{
     console.log(`Server running on part ${port}`);
 });
